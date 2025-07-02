a1dae391dd3ad078b1e2fdf2aff03d85e7ddfd93
Pavel0877 <144724082+Pavel0877@users.noreply.github.com>
2025-07-02 13:48:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 
+import json
+import random
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+
+# Загрузка контента из JSON
+def load_data(filename):
+    try:
+        with open(f"data/{filename}", " "r", encoding="utf-8") as f:
+            return json.load(f)
+    except:
+        return [f"[{filename}] файл не найден или ошибка чтения"]
+
+jokes = load_data("jokes.json")
+anekdots = load_data("anekdots.json")
+pozdravleniya = load_data("pozdravleniya.json")
 
 main_menu_keyboard = [['Шутки'], ['Анекдоты'], ['Поздравления']]
 back_keyboard = [['Старт'], ['Назад в меню']]
@@ -27,9 +41,13 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text("Жми 'Старт', чтобы получить поздравление.",
                                         reply_markup=ReplyKeyboardMarkup(back_keyboard, resize_keyboard=True))
     elif text == 'Старт':
-        category = context.user_data.get('category', None)
-        if category:
-            await update.message.reply_text(f"[{category}] Заглушка: тут будет контент.")
+        category = context.user_data.get('category')
+        if category == 'jokes':
+            await update.message.reply_text(random.choice(jokes))
+        elif category == 'anekdots':
+            await update.message.reply_text(random.choice(anekdots))
+        elif category == 'pozdravleniya':
+            await update.message.reply_text(random.choice(pozdravleniya))
         else:
             await update.message.reply_text("Сначала выбери категорию.")
     elif text == 'Назад в меню':
